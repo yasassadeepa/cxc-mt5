b87a5a7a019b677565da10cc04dbe994d667095d
Yasas Sadeepa <yasassadeepa868@gmail.com>
2024-08-08 11:45:53 +0530

    Exception Handling for read_config_file

--- functions/configs.py
+++ functions/configs.py
@@ -1,9 +1,14 @@
 # Function to read configuration from a text file
 def read_config_file(filename):
     config = {}
-    with open(filename, 'r') as file:
-        lines = file.readlines()
-        for line in lines:
-            key, value = line.strip().split('=')
-            config[key] = value
+    try:
+        with open(filename, 'r') as file:
+            lines = file.readlines()
+            for line in lines:
+                key, value = line.strip().split('=')
+                config[key] = value
+    except FileNotFoundError:
+        print(f"Configuration file {filename} not found.")
+    except Exception as e:
+        print(f"Error reading configuration file: {e}")
     return config
