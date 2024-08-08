0c904602717a5709e0ae84186b24a3480015152e
Yasas Sadeepa <yasassadeepa868@gmail.com>
2024-08-08 16:11:53 +0530

    apply types for schedule_tasks

--- functions/scheduler.py
+++ functions/scheduler.py
@@ -4,7 +4,7 @@ from functions.trading import run_get_previous_day_high_low, run_get_previous_as
 from functions.session import delete_pending_orders_at_1am, adjust_sl_tp
 
 # Function to schedule tasks
-def schedule_tasks(currency_pairs, day_high_low_time, asia_high_low_time, delete_orders_time, lot_size):
+def schedule_tasks(currency_pairs:list, day_high_low_time:str, asia_high_low_time:str, delete_orders_time:str, lot_size:float):
     # Schedule get_previous_day_high_low at the specified time
     schedule.every().day.at(day_high_low_time).do(lambda: run_get_previous_day_high_low(currency_pairs, lot_size))
 
